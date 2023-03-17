d04629182d5d95e3024e77eb8b5cd9cf39ee1535
Tran Ngoc Minh Hieu <tnmhieu@gmail.com>
2023-03-17 21:18:42 +0700

    Create setup.py

---
+++ geoapivietnam/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='geoapivietnam',
+    version='0.1.0',
+    description='A short description of your module',
+    author='Tran Ngoc Minh Hieu',
+    author_email='tnmhieu@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'requests',
+        'geopy',
+        'tenacity'
+        'os'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
