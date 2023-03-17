f38f6ae37c7ff5856e66ca064cc5b1eaf968b934
Tran Ngoc Minh Hieu <tnmhieu@gmail.com>
2023-03-18 00:31:05 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='geoapivietnam',
-    version='0.1.0',
+    version='0.2.0',
     description='Geo API for Vietnam module',
     url='https://github.com/tranngocminhhieu/geo-api-vietnam',
     author='Tran Ngoc Minh Hieu',
