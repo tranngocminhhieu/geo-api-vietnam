6ccf54b989bdc32b4279ef7347820bee2b3652cc
Tran Ngoc Minh Hieu <tnmhieu@gmail.com>
2023-03-19 23:24:40 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url='https://github.com/tranngocminhhieu/geo-api-vietnam',
     author='Tran Ngoc Minh Hieu',
     author_email='tnmhieu@gmail.com',
-    packages=['geoapivietnam'],
+    packages=['src/geoapivietnam'],
     package_data={'geoapivietnam': ['data/*']},
     install_requires=[
         'pandas',
