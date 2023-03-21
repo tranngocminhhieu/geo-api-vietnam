f1a8ca032d163af3b969dbc76a35e9d624033336
Tran Ngoc Minh Hieu <tnmhieu@gmail.com>
2023-03-21 22:31:53 +0700

    Fix bug

--- geoapivietnam/main.py
+++ geoapivietnam/main.py
@@ -221,10 +221,8 @@ class GetLocation:
         province = None # Set default if not match
 
         for part in address_split:
-            print(part)
             for o, a in zip(self.df_valid_provinces.original_province, self.df_valid_provinces.alias_province):
                 a_ul = unidecode(str(a).lower())
-                print(part, '|', a_ul)
                 if a_ul in part:
                     province = o
                     break
